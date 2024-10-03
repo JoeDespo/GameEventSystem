77329a60d6aabc13a848bde3e4a76e42332f3ea2
JoeDespo <jdesposito17@gmail.com>
2024-10-03 12:13:43 -0400

    Modified the original file and added the requested code.

--- singleton_game_engine.cpp
+++ singleton_game_engine.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
 #include <string>
 
+class GameEngine
+{
+    private:
+    static GameEngine* instance; //Holds the only instance
+
+    GameEngine()
+    {
+        std::cout << "GameEngine created!" << std::endl;
+    }
+
+    public:
+    //This method gives access to the instance
+    static GameEngine* getInstance()
+    {
+        if (!instance)
+        {
+            std::cout << "Creating a GameEngine instance" << std::endl;
+            instance = new GameEngine();
+        }
+        else
+        {
+            std::cout << "Returning existing instance of GameEngine..." << std::endl;
+        }
+        return instance;
+    }
+
+    //Notify player the new game event
+    void triggerEvent(const std::string& event)
+    {
+        std::cout << "[GameEngine] Event Triggered: " << event << std::endl;
+    }
+
+    //Notify player on player actions
+    void playerAction(const std::string& player, const std::string& action)
+    {
+        std::cout << "[GameEngine] " << player << " " << action << std::endl;
+    }
+
+};
+
+// Initialize the static instance variable to nullptr
+GameEngine* GameEngine::instance = nullptr;
 
 
-int main() {
+int main()
+{
     GameEngine* engine = GameEngine::getInstance();
 
     // Trigger game events
@@ -23,7 +66,7 @@ int main() {
 
 /**
  * Expected Output:
- * 
+ *
  * [GameEngine] Event Triggered: Enemy appeared!
  * [GameEngine] Event Triggered: Treasure found!
  * [GameEngine] Player1 attacks the enemy.
